92269addbae6d66a368a19001f265599d88f5aaf
collinetjorge <collinetjorge@gmail.com>
2014-10-14 20:29:56 +0000

    solo subo mi lista de amigos despues el resto es de testing de subida

---
+++ src/pack/pruebas.py
@@ -0,0 +1,26 @@
+'''
+Created on 14/10/2014
+
+
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testHola(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testHola']
+    unittest.main()
