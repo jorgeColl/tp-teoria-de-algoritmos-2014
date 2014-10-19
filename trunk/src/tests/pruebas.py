dc02b0f86fb8a06e1d52c58a84f7fb964dd4ce13
collinetjorge@gmail.com <collinetjorge@gmail.com@8ce4bca3-cc29-f42f-780e-969eee3ec64e>
2014-10-19 02:19:13 +0000

    mas tests a masInfluyente

--- trunk/src/tests/pruebas.py
+++ trunk/src/tests/pruebas.py
@@ -23,6 +23,11 @@ class Test(unittest.TestCase):
         maspopular = masPopular(red)
         self.assertEqual(maspopular.getId(),"5" ,maspopular.getLabel())
     
+    def testMasPopular2(self):
+        red = main.cargarRedDesdeArchivo("amigosPruebaEnunciadoTp.gdf")
+        nodoMasPopular = masPopular(red)
+        self.assertEqual(nodoMasPopular.getLabel(), "Juana", "no coinciden los mas inlfuyentes, lo obtenido fue "+nodoMasPopular.getLabel())
+        
     def testMasInfluyente1(self):
         red = main.cargarRedDesdeArchivo("amigosPrueba2.gdf")
         nodoMasInfluyente = masInfluyente(red)
@@ -34,6 +39,14 @@ class Test(unittest.TestCase):
         self.assertEqual(nodoMasInfluyente.getLabel(), "LUIS", "no coinciden los mas inlfuyentes")
         
     def testMasInfluyente3(self):
+        #este test corrobora lo que mandaron por mail del resultado del ejercicio de ejemplo en
+        # el enunciado del tp
+        red = main.cargarRedDesdeArchivo("amigosPruebaEnunciadoTp.gdf")
+        nodoMasInfluyente = masInfluyente(red)
+        self.assertEqual(nodoMasInfluyente.getLabel(), "Juana", "no coinciden los mas inlfuyentes se obtuvo: "+nodoMasInfluyente.getLabel())
+
+            
+    def testMasInfluyente4(self):
         red = main.cargarRedDesdeArchivo("amigosPrueba1.gdf")
         nodoMasInfluyente = masInfluyente(red)
         #TODAVIA TENGO QUE HACER LA CUENTA DE SI ESTA BIEN EL RESULTADO
@@ -41,7 +54,45 @@ class Test(unittest.TestCase):
                 
     
     def testSujerirAmigo(self):
+        red = main.cargarRedDesdeArchivo("amigosPruebaEnunciadoTp.gdf")
         pass
+        """ LO QUE DIJIERON QUE EL EJEMPLO DEBERIA DEVOLVER(SEGUN EL MAIL QUE MANDARON)
+        -----------------recomendaciones-----------------
+        Roberto: Esteban (2 amigo(s) en comun)
+        Milena: Monica (2 amigo(s) en comun)
+        Milena: Carlos (2 amigo(s) en comun)
+        Monica: Milena (2 amigo(s) en comun)
+        Carlos: Milena (2 amigo(s) en comun)
+        Esteban: Roberto (2 amigo(s) en comun)
+        Juana: Milena (1 amigo(s) en comun)
+        Juana: Monica (1 amigo(s) en comun)
+        Juana: Esteban (1 amigo(s) en comun)
+        Juana: Pablo (1 amigo(s) en comun)
+        Juana: Brenda (1 amigo(s) en comun)
+        Pablo: Roberto (1 amigo(s) en comun)
+        Pablo: Monica (1 amigo(s) en comun)
+        Pablo: Juana (1 amigo(s) en comun)
+        Pablo: Tomas (1 amigo(s) en comun)
+        Lorena: Milena (1 amigo(s) en comun)
+        Lorena: Monica (1 amigo(s) en comun)
+        Lorena: Carlos (1 amigo(s) en comun)
+        Lorena: Tomas (1 amigo(s) en comun)
+        Lorena: Brenda (1 amigo(s) en comun)
+        Lorena: Nora (1 amigo(s) en comun)
+        Tomas: Roberto (1 amigo(s) en comun)
+        Tomas: Esteban (1 amigo(s) en comun)
+        Tomas: Pablo (1 amigo(s) en comun)
+        Tomas: Lorena (1 amigo(s) en comun)
+        Tomas: Nora (1 amigo(s) en comun)
+        Brenda: Milena (1 amigo(s) en comun)
+        Brenda: Juana (1 amigo(s) en comun)
+        Brenda: Esteban (1 amigo(s) en comun)
+        Brenda: Lorena (1 amigo(s) en comun)
+        Nora: Roberto (1 amigo(s) en comun)
+        Nora: Carlos (1 amigo(s) en comun)
+        Nora: Lorena (1 amigo(s) en comun)
+        Nora: Tomas (1 amigo(s) en comun)
+        """
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testMasPopular']
