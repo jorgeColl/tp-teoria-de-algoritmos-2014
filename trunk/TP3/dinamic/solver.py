63378cbf9daa470bc612bb818ab3f45b0b78fbad
collinetjorge@gmail.com <collinetjorge@gmail.com@8ce4bca3-cc29-f42f-780e-969eee3ec64e>
2014-11-27 02:47:56 +0000

    mejore documentacion, solo falta testear un poco mas (1 o 2 pruebas mas) ya esta solido

--- trunk/TP3/dinamic/solver.py
+++ trunk/TP3/dinamic/solver.py
@@ -20,7 +20,7 @@ class Solver(object):
         self.maxD = self.listaTareas[len(self.listaTareas) - 1][2]
         self.tareaN = len(self.listaTareas)
         
-        # genero la matriz de valores
+        # inicializo la matriz de valores vacia
         for x in range (0, self.tareaN + 1):
             aux = [0] * (self.maxD + 1)
             self.Matrix.append(aux)
@@ -33,8 +33,10 @@ class Solver(object):
             return 1
         return -1
     
+    """ genera la matriz de valores, esta tiene como informacion que valor optimo existe
+    para un sub problema con vencimiento maximo d y tareas a """
+    """Orden getMatrixValues: O(a*v)"""
     def getMatrixValues(self):
-        """Orden: O(a*v)"""
         for a in range(1, self.tareaN + 1):
             for v in range(0, self.listaTareas[a-1][2] + 1):
                 t = self.listaTareas[a - 1][0]
@@ -53,14 +55,14 @@ class Solver(object):
                
     """ a partir de la matriz de valores obtiene las tareas correspondientes de la
     solucion optima """
+    """ Orden __getPathSolution: O(a) """
     def __getPathSolution(self):
-        """ como se puede observar se iterara en el while la cantidad de tareas que existan O(a)"""
         solucionTuplas = []
         a = len(self.listaTareas)
         v = self.maxD
         
-        while (self.Matrix[a][v] !=0 and a != 0 and v != 0 ):
-            #while(self.Matrix[a][v] == self.Matrix[a][v-1]):
+        while (self.Matrix[a][v] != 0 and a != 0 and v != 0):
+            # while(self.Matrix[a][v] == self.Matrix[a][v-1]):
             #    v-=1
             t = self.listaTareas[a - 1][0]
             b = self.listaTareas[a - 1][1]
@@ -73,7 +75,7 @@ class Solver(object):
                 a -= 1
             else:
                 """es tarea optima"""
-                solucionTuplas.append(self.listaTareas[a-1])
+                solucionTuplas.append(self.listaTareas[a - 1])
                 a -= 1
                 v = v - t
                 
@@ -85,7 +87,9 @@ class Solver(object):
             
         return solucionTareas
     
-    """ orden O(a*v + a)"""
+    """ orden solve:  O(a*v + a) 
+        con a: cantidad de tareas
+        con v: vencimiento maximo   """
     def solve(self):
         self.getMatrixValues()
         return self.__getPathSolution()
